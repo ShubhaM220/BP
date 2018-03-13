97a6298ac1e60ae957caa0d446ec67edcbdada56
ShubhaM220 <shubham.boko31@gmail.com>
2018-03-13 17:25:13 +0530

    [Harman]
    Signed-off-by: ShubhaM220 <shubham.boko31@gmail.com>

--- test/test.c
+++ test/test.c
@@ -3,5 +3,6 @@
 int main(){
 	printf("some test has been added\n");
 	printf("some test has been added\n");
+	printf("some test has been added\n");
 	return 0;
 }
--- test/test_1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main(){
-	printf("some test has been added\n");
-	return 0;
-}
